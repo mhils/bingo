e753ffab93a02e98a60e5d5f5a1f767cf711ab88
Maximilian Hils <git@maximilianhils.com>
2022-08-31 05:03:01 +0200

    update

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "bingo-v1";
+const CACHE_URLS = [
+  "./",
+  "./bootstrap.min.css",
+  "./play.html",
+  "./textFit.js",
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+  	const cache = await caches.open(CACHE_NAME);
+  	await cache.addAll(CACHE_URLS);
+  })());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    if (r) {
+    	return r;
+    }
+    const response = await fetch(e.request);
+    // const cache = await caches.open(CACHE_NAME);
+    // cache.put(e.request, response.clone());
+    return response;
+  })());
+});
